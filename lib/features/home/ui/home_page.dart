bad90ff703dd19445741c4a9a55d2cd7d2597f7c
Fernando Luis <fernando.martinez@soytul.com>
2022-09-25 20:59:01 -0500

    Feat: Add gallery page

--- lib/features/home/ui/home_page.dart
+++ lib/features/home/ui/home_page.dart
@@ -1,28 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:image_gallery_from_url/features/upload/ui/upload_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:image_gallery_from_url/di/dependency_injection.dart';
+import 'package:image_gallery_from_url/features/gallery/gallery_page.dart';
+import 'package:image_gallery_from_url/features/upload/ui/bloc/upload_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: const SafeArea(
-        child: UploadPage(),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.cloud_upload_outlined),
-            label: 'Subir Imagen',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo_library_outlined),
-            label: 'Galería',
-          ),
-        ],
-        currentIndex: 0,
-        onTap: (_) {},
+    return BlocProvider(
+      create: (context) => UploadBloc(sl())
+        ..add(
+          LoadImagesEvent(),
+        ),
+      child: Scaffold(
+        body: const SafeArea(
+          child: GalleryPage(),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.cloud_upload_outlined),
+              label: 'Subir Imagen',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo_library_outlined),
+              label: 'Galería',
+            ),
+          ],
+          currentIndex: 0,
+          onTap: (_) {},
+        ),
       ),
     );
   }
